185ed589de2c5f987a4c47a0b5a9c2c14be85b2e
Albert Nisbet <albertnis@gmail.com>
2023-07-18 21:58:24 +1200

    Add format function

--- index.ts
+++ index.ts
@@ -15,18 +15,31 @@ type I18nArgNameTypeofExample = I18nArgName<"name, string">;
 
 type I18nArgs<Template extends string> = string extends Template
   ? // Disregard non-literal string
-    string[]
-  : // Is there nothing left to process?
-  Template extends ""
-  ? {}
+    never
   : // Look at the next {arg} in curly braces
   Template extends `${infer _}{${infer Arg}}${infer Rest}`
   ? // Add the arg by its name to an object
     { [k in I18nArgName<Arg>]: I18nArgTypeof<Arg> } & I18nArgs<Rest>
   : {};
 
-type I18nArgsExample =
-  I18nArgs<"Hello {otherName}! My name is {myName} and it's nice to meet you. We first met on {meetingDate, date}">;
+const strings = {
+  title: "Hyper I18n",
+  donation: "Donate ${amount, number, currency} to our project",
+  greeting:
+    "Hello {otherName}! My name is {myName} and it's nice to meet you. We first met on {meetingDate, date}",
+} as const;
+
+type Strings = typeof strings;
+type I18nMessageKey = keyof Strings;
+
+const f = <T extends I18nMessageKey>(
+  messageKey: T,
+  args: I18nArgs<Strings[T]>
+) => {};
+
+f("donation", { amount: 30 });
+
+type I18nArgsExample = I18nArgs<(typeof strings)["greeting"]>;
 
 const vars: I18nArgsExample = {
   myName: "",
