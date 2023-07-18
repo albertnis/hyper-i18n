5b315da29a800a583d346732ba60afe2d3c0e662
Albert Nisbet <albertnis@gmail.com>
2023-07-18 22:28:17 +1200

    Make it work for tags

--- index.ts
+++ index.ts
@@ -22,11 +22,32 @@ type I18nArgs<Template extends string> = string extends Template
     { [k in I18nArgName<Arg>]: I18nArgTypeof<Arg> } & I18nArgs<Rest>
   : {};
 
+type I18nTags<
+  Template extends string,
+  Output = string
+> = string extends Template
+  ? // Disregard non-literal string
+    never
+  : // Look at the next {arg} in curly braces
+  Template extends `${infer _}<${infer Tag}>${infer Child}</${infer Tag}>${infer Rest}`
+  ? // Add the arg by its name to an object
+    { [k in Tag]: (child: string) => Output } & I18nArgs<Rest>
+  : {};
+
+type I18nMessageParameters<
+  Template extends string,
+  Output = string
+> = I18nArgs<Template> & I18nTags<Template, Output>;
+
 const strings = {
   title: "Hyper I18n",
   donation: "Donate ${amount, number, currency} to our project",
   greeting:
     "Hello {otherName}! My name is {myName} and it's nice to meet you. We first met on {meetingDate, date}",
+  tagged:
+    "<b>This entire sentence is bold and has a link to my site: <a>{siteUrl}</a></b>",
+  complex:
+    "You have {notificationCount, plural, =0 {no notifications} one {# notification} other {# notifications}}",
 } as const;
 
 type Strings = typeof strings;
@@ -34,15 +55,19 @@ type I18nMessageKey = keyof Strings;
 
 const f = <T extends I18nMessageKey>(
   messageKey: T,
-  args: I18nArgs<Strings[T]>
+  args: I18nMessageParameters<Strings[T]>
 ) => {};
 
-f("donation", { amount: 30 });
+f("tagged", {
+  b: (child) => `**${child}**`,
+  a: (child) => `[](${child})`,
+  siteUrl: "https://example.com",
+});
 
-type I18nArgsExample = I18nArgs<(typeof strings)["greeting"]>;
+type I18nMessageParametersExample = I18nMessageParameters<
+  (typeof strings)["tagged"]
+>;
 
-const vars: I18nArgsExample = {
-  myName: "",
-  otherName: "",
-  meetingDate: new Date(),
-};
+type NotArray = { 0: any; 1: any };
+
+const me: NotArray = [1, 2];
