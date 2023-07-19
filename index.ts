a8c3c6c9fb39ba257f3d8c7926b5f1de91f9afa7
Albert Nisbet <albertnis@gmail.com>
2023-07-19 23:43:20 +1200

    Mag tags and args more robust

--- index.ts
+++ index.ts
@@ -1,26 +1,89 @@
-type I18nArgName<Arg extends string> = Arg extends `${infer ArgName},${infer _}`
-  ? ArgName
-  : Arg;
+type I18nArgTypeof<ArgType extends string> = ArgType extends `number`
+  ? number
+  : ArgType extends `date`
+  ? Date
+  : string;
 
-type I18nArgNameExample = I18nArgName<"name, string">;
+type IcuArgType =
+  | "number"
+  | "date"
+  | "time"
+  | "spellout"
+  | "ordinal"
+  | "duration";
 
-type I18nArgTypeof<Arg extends string> =
-  Arg extends `${infer _}, number${infer __}`
-    ? number
-    : Arg extends `${infer _}, date${infer __}`
-    ? Date
-    : string;
+type StringToNumbers0To9 = {
+  "0": 0;
+  "1": 1;
+  "2": 2;
+  "3": 3;
+  "4": 4;
+  "5": 5;
+  "6": 6;
+  "7": 7;
+  "8": 8;
+  "9": 9;
+};
 
-type I18nArgNameTypeofExample = I18nArgName<"name, string">;
+type Comma = ", ";
+
+type ValidArgNameOrNumber<T extends string> = T extends `${string}${
+  | "}"
+  | ","}${string}`
+  ? never
+  : T extends keyof StringToNumbers0To9
+  ? // If the name is a number 0..9 (as defined by spec) use the corresponding number literal
+    StringToNumbers0To9[T]
+  : T;
+
+type NoneArg<ArgNameOrNumber extends string> =
+  `{${ValidArgNameOrNumber<ArgNameOrNumber>}}`;
+
+type SimpleArg<
+  ArgNameOrNumber extends string,
+  ArgType extends IcuArgType
+> = `{${ValidArgNameOrNumber<ArgNameOrNumber>}${Comma}${ArgType}}`;
+
+type jisdo = SimpleArg<"9", "number">;
+
+type SimpleArgWithStyle<
+  ArgNameOrNumber extends string,
+  ArgType extends IcuArgType,
+  ArgStyle extends string
+> = `{${ValidArgNameOrNumber<ArgNameOrNumber>}${Comma}${ArgType}${Comma}${ArgStyle}}`;
 
 type I18nArgs<Template extends string> = string extends Template
   ? // Disregard non-literal string
     never
-  : // Look at the next {arg} in curly braces
-  Template extends `${infer _}{${infer Arg}}${infer Rest}`
+  : // --- Look for the next simple arg with style
+  Template extends `${infer Before}${SimpleArgWithStyle<
+      infer ArgNameOrNumber,
+      infer ArgType,
+      infer _
+    >}${infer After}`
   ? // Add the arg by its name to an object
-    { [k in I18nArgName<Arg>]: I18nArgTypeof<Arg> } & I18nArgs<Rest>
+    { [k in ArgNameOrNumber]: I18nArgTypeof<ArgType> } & I18nArgs<Before> &
+      I18nArgs<After>
+  : // --- Look for the next simple arg
+  Template extends `${infer Before}${SimpleArg<
+      infer ArgNameOrNumber,
+      infer ArgType
+    >}${infer After}`
+  ? // Add the arg by its name to an object
+    { [k in ArgNameOrNumber]: I18nArgTypeof<ArgType> } & I18nArgs<Before> &
+      I18nArgs<After>
+  : // --- Look for next none tag
+  Template extends `${infer Before}${NoneArg<
+      infer ArgNameOrNumber
+    >}${infer After}`
+  ? // Add the arg by its name to an object
+    { [k in ArgNameOrNumber]: string } & I18nArgs<Before> & I18nArgs<After>
   : {};
+
+type ii =
+  I18nArgs<"You are {name, date} {today, date} and today is {today, date}">;
+
+type ValidTagName<T extends string> = T extends `/${string}` ? never : T;
 
 type I18nTags<
   Template extends string,
@@ -28,11 +91,17 @@ type I18nTags<
 > = string extends Template
   ? // Disregard non-literal string
     never
-  : // Look at the next {arg} in curly braces
-  Template extends `${infer _}<${infer Tag}>${infer Child}</${infer Tag}>${infer Rest}`
-  ? // Add the arg by its name to an object
-    { [k in Tag]: (child: string) => Output } & I18nArgs<Rest>
+  : // Look at the next <tag> in arrow brackets
+  Template extends `${string}<${ValidTagName<infer TagName>}>${infer Rest1}`
+  ? Rest1 extends `${infer Child}</${TagName}>${infer Rest2}`
+    ? // Add the arg by its name to an object
+      { [k in TagName]: (child: string) => Output } & I18nTags<Rest2> &
+        I18nTags<Child>
+    : never
   : {};
+
+type iii = I18nTags<"<a><b></b><c></c><d>wow</d> out</a>">;
+type iai = I18nTags<"<a></a><b></b><c></c>">;
 
 type I18nMessageParameters<
   Template extends string,
@@ -42,8 +111,13 @@ type I18nMessageParameters<
 const strings = {
   title: "Hyper I18n",
   donation: "Donate ${amount, number, currency} to our project",
+  nums: "Your top three items are {0, number}, {1, number}, {1, number} and {2, number}.",
+  spacesInArg:
+    "This {arg name, number} has a space in it which I think is valid {maybe, date}",
+  noSpaceAfterComma:
+    "What about simple args like: {simple,date} which have no space after the comma {withSpace, number} {withoutSpace, duration}",
   greeting:
-    "Hello {otherName}! My name is {myName} and it's nice to meet you. We first met on {meetingDate, date}",
+    "Hello {otherName}, My name is {myName} and it's nice to meet you. We first met on {meetingDate, date, short}",
   tagged:
     "<b>This entire sentence is bold and has a link to my site: <a>{siteUrl}</a></b>",
   complex:
@@ -58,16 +132,8 @@ const f = <T extends I18nMessageKey>(
   args: I18nMessageParameters<Strings[T]>
 ) => {};
 
-f("tagged", {
-  b: (child) => `**${child}**`,
-  a: (child) => `[](${child})`,
-  siteUrl: "https://example.com",
-});
-
 type I18nMessageParametersExample = I18nMessageParameters<
-  (typeof strings)["tagged"]
+  (typeof strings)["noSpaceAfterComma"]
 >;
 
-type NotArray = { 0: any; 1: any };
-
-const me: NotArray = [1, 2];
+f("nums", [0, 2, 3]);
